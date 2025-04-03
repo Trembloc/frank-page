119b6e55f6b3a9355b156e7ae8340ddd521e7e56
Trembloc <tremblay34487@gmail.com>
2025-04-03 14:21:58 -0400

    added a homilies table and a test component to make sure pdf was working

--- src/components/Homilies.jsx
+++ src/components/Homilies.jsx
@@ -2,7 +2,178 @@
 
 const Homilies = () => {
   return (
-    <h2>Homilies</h2>
+    <table>
+      <tr>
+        <th>ADVENT</th>
+        <th>CHRISTMAS TIME</th>
+        <th colspan="2">ORGINARY TIME</th>
+        <th>FEAST DAYS</th>
+        <th>LENT/EASTER</th>
+        <th>OTHERS</th>
+      </tr>
+      <tr>
+        <td>Advent 1</td>
+        <td>Holy Family</td>
+        <td>2nd</td>
+        <td>19th</td>
+        <td>Presentation of the Lord</td>
+        <td>Ash Wednesday</td>
+        <td>Assumption</td>
+      </tr>
+      <tr>
+        <td>Advent 2</td>
+        <td>Epiphany</td>
+        <td>3rd</td>
+        <td>20th</td>
+        <td>Pentacost Vigil</td>
+        <td>1st Sunday of Lent</td>
+        <td>Birth of John the Baptist</td>
+      </tr>
+      <tr>
+        <td>Advent 3</td>
+        <td>Baptism of Lord</td>
+        <td>4th</td>
+        <td>21st</td>
+        <td>Pentacost Sunday</td>
+        <td>2nd Sunday Of Lent</td>
+        <td>Funerals</td>
+      </tr>
+      <tr>
+        <td>Advent 4</td>
+        <td>Christmas</td>
+        <td>5th</td>
+        <td>22nd</td>
+        <td>The Most Holy Trinity</td>
+        <td>3rd Sunday of Lent</td>
+        <td>Mary Mother of God</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Christmas Midnight</td>
+        <td>6th</td>
+        <td>23rd</td>
+        <td>Corpus Christi</td>
+        <td>4th Sunday of Lent</td>
+        <td>Sacred Heart</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Christmas Dawn</td>
+        <td>7th</td>
+        <td>24th</td>
+        <td>Saints Peter & Paul</td>
+        <td>5th Sunday</td>
+        <td>Thanksgiving</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Christmas Vigil</td>
+        <td>8th</td>
+        <td>25th</td>
+        <td>Exaltation of the Holy Cross</td>
+        <td>Holy Thursday</td>
+        <td>Transfiguration</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>9th</td>
+        <td>26th</td>
+        <td>All Souls Day</td>
+        <td>Good Friday</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>10th</td>
+        <td>27th</td>
+        <td>OLJC, King of the Universe</td>
+        <td>Holy Saturday</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>11th</td>
+        <td>28th</td>
+        <td>All Saints Day</td>
+        <td>Palm Sunday</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>12th</td>
+        <td>29th</td>
+        <td></td>
+        <td>Easter Sunday</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>13th</td>
+        <td>30th</td>
+        <td></td>
+        <td>2nd Sunday of Easter</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>14th</td>
+        <td>31st</td>
+        <td></td>
+        <td>3rd Sunday of Easter</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>15th</td>
+        <td>32nd</td>
+        <td></td>
+        <td>4th Sunday of Easter</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>16th</td>
+        <td>33rd</td>
+        <td></td>
+        <td>5th Sunday of Easter</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>17th</td>
+        <td>34th</td>
+        <td></td>
+        <td>6th Sunday of Easter</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>18th</td>
+        <td></td>
+        <td></td>
+        <td>7th Sunday of Easter</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>Ascension Sunday</td>
+        <td></td>
+      </tr>
+    </table>
   )
 }
 
