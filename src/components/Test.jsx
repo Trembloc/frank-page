d34d336d9d61c741055605e5e87f44526c7c0f8a
Trembloc <tremblay34487@gmail.com>
2025-04-22 16:02:39 -0400

    import method

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,7 +1,9 @@
+import pdfUrl from '../../Homilies/Advent1stCa.pdf'
+
 const Test = () => {
-const pdfPath = process.env.NODE_ENV === "production" ? "../Homilies/Advent1stCa.pdf" : "./Homilies/Advent1stCa.pdf"
+//const pdfPath = process.env.NODE_ENV === "production" ? "../Homilies/Advent1stCa.pdf" : "./Homilies/Advent1stCa.pdf"
   return (
-    <iframe id="pdf" src={pdfPath} frameborder="0"></iframe>
+    <iframe id="pdf" src={pdfUrl} frameborder="0"></iframe>
   )
 }
 
