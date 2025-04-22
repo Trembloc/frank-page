33c4d7adc37adfaf0c6080f5bc5e9c4575ee7b11
Trembloc <tremblay34487@gmail.com>
2025-04-22 15:48:20 -0400

    dist path

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,5 +1,5 @@
 const Test = () => {
-const pdfPath = process.env.NODE_ENV === "production" ? "../../Homilies/Advent1stCa.pdf" : "./Homilies/Advent1stCa.pdf"
+const pdfPath = process.env.NODE_ENV === "production" ? "../Homilies/Advent1stCa.pdf" : "./Homilies/Advent1stCa.pdf"
   return (
     <iframe id="pdf" src={pdfPath} frameborder="0"></iframe>
   )
