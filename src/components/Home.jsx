287b778672dc14814de9b158988bc608c6f0fc24
Trembloc <tremblay34487@gmail.com>
2025-04-24 15:05:35 -0400

    added text to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,52 @@
 const Home = () => {
 
-  return(
-    <h2>HOME</h2>
+  return (
+    <>
+      <h1>HOME</h1>
+      <section>
+        <h2>The Homilies of Deacon Frank</h2>
+        <p>In his 20 years as an ordained Deacon of the Roman Catholic Church, Deacon Frank’s greatest passion
+          and his greatest gift was sharing the Word of the Lord.
+
+          Before Frank returned to the Lord, we are fortunate that he was able to share his saved speaking notes for
+          almost 500 sermons that he delivered through the years. Some of these sermons were delivered to an
+          overflowing church on Easter Sunday. Others were delivered to a bare handful at a makeshift Mass in a
+          small classroom while on mission in the Bahamas. However, all of these sermons were delivered with a
+          passion to share the Word of God with any that would listen.
+
+          This is a record of the speaking notes Deacon Frank made for himself to present his homilies. We
+          unfortunately do not have any recordings or transcripts that would give us the gift of hearing his voice.
+          What we do have are the speaking notes he used to share the insights he offered into the Readings of the
+          day.
+
+          While we can not hear the stories Deacon Frank told us on Sunday’s, hopefully the opportunity to see into
+          his speaking notes will provide you with a chance to gain an insight into that day’s Readings that you may
+          not have had. Deacon Frank’s personal notes are provided here for all to have an opportunity to continue to
+          learn from his unique way of spreading The Word.</p>
+      </section>
+      <section>
+        <h2>Obituary for Deacon Francis "Frank" R. Tremblay</h2>
+        <p>TREMBLAY, FRANCIS R. “Deacon Frank” of Stoughton passed away on January 31, 2025 at the age of
+          75. Beloved husband of the late Virginia “Ginni” M. (Caldwell) Tremblay. Devoted father of Matthew D.
+          Tremblay and his wife Melissa of Kingwood, TX, Michael F. Tremblay and his wife Catherine of East
+          Hampton, NY and Joseph D. Tremblay and his wife Liza of Sag Harbor, NY. Brother of Rosanna Kenney of
+          Dracut and Mary Burke of Methuen. Cherished grandfather of Jack Christopher Tremblay, Leo John
+          Tremblay, Rose W. Tremblay and Henri S. Tremblay. Also survived by many nieces and nephews. Son of
+          the late Leo J. and Gloria A. (Boucher) Tremblay.
+          Frank was a proud graduate of Boston College and had a long career in technology while living with his
+          family in Canton, MA. Always a devout Catholic, Frank was an active member of the New Jerusalem
+          Charismatic Prayer Group, meeting at St. Timothy’s Parish in Norwood for 40 years. In 2002 he was
+          ordained a Deacon and transitioned from a career in technology to managing the Sacred Hearts Retreat
+          Center in Wareham. Upon retiring, Frank and his wife Ginni moved to Andros Island in the Bahamas where
+          they established a Catholic ministry and served the community for seven years. They were forced to return
+          to the US when Ginni began to show the signs of Alzheimer’s and Frank cared for her at their home in
+          Stoughton for many years while ministering at Our Lady of Sorrows in Sharon, the Chaplain at the Nassau
+          Street Jail, and finally as deacon at St. Catherine’s of Siena in Norwood. He was blessed to live a life
+          touched by many wonderful friends and was especially fortunate to have found Dr. Marjorie Goodwin who
+          was a cherished companion after the death of his wife and supported him through his battle with cancer.</p>
+      </section>
+    </>
+
   )
 }
 
